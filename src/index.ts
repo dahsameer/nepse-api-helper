22b6e04187b1261d2598c7b90c1f8408e224e3b1
Sameer Dahal <dahsameer@gmail.com>
2022-04-17 19:51:38 +0545

    fixed issue with getting security details

--- src/index.ts
+++ src/index.ts
@@ -84,19 +84,28 @@ async function UpdateSecurityBriefs() {
   securityBriefList = await GetSecurityBriefs();
 }
 
+function GetValidBodyId(marketId: number){
+  const dummyData = [147, 117, 239, 143, 157, 312, 161, 612, 512, 804, 411, 527, 170, 511, 421, 667, 764, 621, 301, 106, 133, 793, 411, 511, 312, 423, 344, 346, 653, 758, 342, 222, 236, 811, 711, 611, 122, 447, 128, 199, 183, 135, 489, 703, 800, 745, 152, 863, 134, 211, 142, 564, 375, 793, 212, 153, 138, 153, 648, 611, 151, 649, 318, 143, 117, 756, 119, 141, 717, 113, 112, 146, 162, 660, 693, 261, 362, 354, 251, 641, 157, 178, 631, 192, 734, 445, 192, 883, 187, 122, 591, 731, 852, 384, 565, 596, 451, 772, 624, 691];
+  const currentDate = new Date();
+  const datePart = currentDate.getDate();
+  const id = dummyData[marketId] + marketId + 2 * datePart;
+  return id;
+}
+
 async function GetSecurityDetail(symbol: string): Promise<SecurityDetail | null> {
   const security = securityBriefList.find(s => s.symbol.toLowerCase() === symbol.toLowerCase());
   if (!security) {
     return null;
   }
   const marketStatus = await GetMarketStatus();
+  const bodyId = GetValidBodyId(marketStatus?.id ?? 0);
   const token = await GetAccessToken();
   const securityDetail: SecurityDetail | null = await fetch(`${BASE_URL}/nots/security/${security.id}`, {
     headers: {
       authorization: `Salter ${token}`,
       "content-type": "application/json",
     },
-    body: `{"id": ${marketStatus?.id}}`,
+    body: `{"id": ${bodyId}}}`,
     method: "POST",
   })
     .then(resp => resp.json() as Promise<SecurityDetailResponse>)
